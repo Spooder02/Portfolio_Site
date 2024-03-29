21b9494b73cb3f1f4de076eb4041363435cdc7ab
Spooder <yoshi7591@naver.com>
2024-03-29 15:49:36 +0900

    프로젝트 삭제 구현

--- portfolio_backend/src/main/java/com/spooderportfolio/demo/Controller/ProjectController.java
+++ portfolio_backend/src/main/java/com/spooderportfolio/demo/Controller/ProjectController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -19,7 +20,6 @@ import com.spooderportfolio.demo.Service.ProjectService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -44,4 +44,9 @@ public class ProjectController {
         return projectService.getAllProject();
     }
     
+    @DeleteMapping
+    public ResponseEntity<String> deleteProject(@RequestBody Long id) {
+        projectService.deleteProject(id);
+        return ResponseEntity.ok("Successfully deleted");
+    }
 }
